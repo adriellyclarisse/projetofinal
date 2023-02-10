fa8ccfa5faefe6d9d56aa1a9222b7cc1e3902e25
Adrielly <adriellyclarisse2018@gmail.com>
2023-02-10 11:32:55 -0300

    quase lá

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -3,13 +3,17 @@ package ifrn.pi.eventos.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ifrn.pi.eventos.models.Evento;
 import ifrn.pi.eventos.repositories.EventoRepository;
@@ -22,17 +26,23 @@ public class EventosController {
 	private EventoRepository er;
 	
 	@GetMapping("/form")
-	public String form() {
+	public String form(Evento evento) {
 		return "eventos/formEvento";
 	}
 	
 	@PostMapping
-	public String adicionar(Evento evento) {
+	public String salvar(@Valid Evento evento, BindingResult result, RedirectAttributes attributes) {
+		
+		if(result.hasErrors()){
+			return form(evento);
+			
+		}
 		
 		System.out.println(evento);
 		er.save(evento);
+		attributes.addFlashAttribute("mensagem", "Seu evento foi alterado com sucesso!");
 		
-		return "eventos/evento-adicionado";
+		return "redirect:/eventos";
 	}
 	
 	@GetMapping
@@ -59,6 +69,39 @@ public class EventosController {
 		md.addObject("evento", evento);
 		
 		return md;
+		
+	}
+	
+	@GetMapping("/{id}/selecionar")
+	public ModelAndView selecionarEvento(@PathVariable Long id) {
+		ModelAndView md = new ModelAndView();
+		Optional<Evento> opt = er.findById(id);
+		if(opt.isEmpty()){
+			md.setViewName("redirect:/eventos");
+			return md;
+		}
+		
+		Evento evento = opt.get();
+		md.setViewName("eventos/formEvento");
+		md.addObject("evento", evento);
+
+		return md;
+
+	}
+
+	
+	@GetMapping("/{id}/remover")
+	public String apagarEvento(@PathVariable Long id, RedirectAttributes attributes) {
+		
+		Optional<Evento> opt = er.findById(id);
+		
+		if(!opt.isEmpty()) {
+			Evento evento = opt.get();
+			er.delete(evento);
+			attributes.addFlashAttribute("mensgem", "Evento removido com sucesso!");
+		}
+		
+		return "redirect:/eventos";
 		
 	}
 	
